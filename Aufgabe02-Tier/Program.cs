d605e124a3271ee22920f677e707dacab3e5d411
Sanji <chokpichit@poonpin.de>
2024-05-28 17:56:07 +0200

    Tier and Program first commit

---
+++ Aufgabe02-Tier/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Aufgabe02_Tier;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tier[] meinKleinerZoo = new Tier[]
+            {
+                new Hund("Bobo"),
+                new Katze("Sushi"),
+                new Maus("Jerry")
+            };
+
+        foreach (var tier in meinKleinerZoo)
+        {
+            tier.MacheAufDichAufmerksam();
+        }
+    }
+
+}
+
---
+++ Aufgabe02-Tier/Tier.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Aufgabe02_Tier
+{
+	public abstract class Tier
+	{
+		public string Name { get; set; }
+
+		public abstract void MacheAufDichAufmerksam();
+
+        public Tier(string name)
+        {
+            Name = name;
+        }
+    }
+
+	public class Hund : Tier
+	{
+        public Hund(string name) : base(name) { }
+
+        public override void MacheAufDichAufmerksam()
+		{
+			Console.WriteLine("Ich bin ein Hund und mache Wau"); ;
+		}
+    }
+
+	public class Katze : Tier
+	{
+        public Katze(string name) : base(name) { }
+
+        public override void MacheAufDichAufmerksam()
+        {
+            Console.WriteLine("Ich bin eine Katze und mache Miau"); 
+        }
+    }
+
+	public class Maus : Tier
+	{
+        public Maus(string name) : base(name) { }
+
+        public override void MacheAufDichAufmerksam()
+        {
+            Console.WriteLine("Ich bin eine Maus und mache Piep"); 
+        }
+    }
+
+
+}
+
